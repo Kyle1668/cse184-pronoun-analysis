55eb60fb4ca708fbc7a0a0514d6eaac4290a1e3c
kyle1668 <kyledevinobrien1@gmail.com>
2019-11-12 18:19:53 -0800

    added corpus of masculine wording and gender pronouns

---
+++ data/corpus.py
@@ -0,0 +1,29 @@
+gender_pronouns = [
+    "he",
+    "him",
+    "she",
+    "her",
+    "his",
+    "hers"
+]
+
+masculine_themed_wording = [
+    "competitive",
+    "dominate",
+    "leader",
+    "rock star",
+    "rockstar",
+    "guru",
+    "ninja",
+    "hacker",
+    "superhero",
+    "prove themselves",
+    "analyze",
+    "determine",
+    "crush it",
+    "world class",
+    "superior",
+    "ambitious",
+    "aggressive",
+    "leader"
+]
