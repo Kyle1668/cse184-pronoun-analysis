fdd58958c346ce2a065cf61a2b3aaf3860542b86
kyle1668 <kyledevinobrien1@gmail.com>
2019-11-20 14:49:49 -0800

    got national header to load from dumym data

--- visualization/src/components/PageHeader.js
+++ visualization/src/components/PageHeader.js
@@ -1,25 +1,21 @@
 import React, { Component } from "react";
 import NationalStatCard from "./NationalStatCard";
-import data from "../data.json";
+import "../styles/PageHeader.css";
+import data from "../fake-data.json";
 
 export default class PageHeader extends Component {
   render() {
     return (
       <div className="page-header">
         <h1>Gendered Job Posting Analysis</h1>
-        <NationalStatCard
-          question="What percentage have gender pronouns in the job posting?"
-          answer="15%"
-        />
-        {/* <NationalStatCard question="How are gendered job postings distributed across the USA?" answer="lorem ipsum" /> */}
-        <NationalStatCard
-          question="What are the most common roles in tech that have gendered job postings?"
-          answer="SWE, PM, and CTO"
-        />
-        <NationalStatCard
-          question="Out of the gendered words/phrases, which are the most common? "
-          answer="hacker, ninja, dominate"
-        />
+        {data.USA.map(element => {
+          return (
+            <NationalStatCard
+              question={element.question}
+              answer={element.answer}
+            />
+          );
+        })}
       </div>
     );
   }
