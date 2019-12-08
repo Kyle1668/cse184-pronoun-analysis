0b159976ef9c7b26a68ae766f63daeb0e835855d
nikhildodd <44418217+nikhildodd@users.noreply.github.com>
2019-12-08 15:17:43 -0800

    Added more to the list of phrases using stemming

--- data/corpus.py
+++ data/corpus.py
@@ -27,3 +27,5 @@ masculine_themed_wording = [
     "aggressive",
     "leader"
 ]
+s = "active adventurous aggress ambitio analy assert athlet autonom battle boast challeng champion compet confident courag decid decision decisive defend determin domina dominant driven fearless fight force greedy head-strong headstrong hierarch hostil impulsive independen individual intellect lead logic objective opinion outspoken persist principle reckless self-confiden self-relian self-sufficien selfconfiden selfrelian selfsufficien stubborn superior unreasonab"
+masculine_themed_wording_updated = s.split()
